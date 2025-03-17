ea6b6fa759ca18815e604d2c1b8adc23c9938e2c
clarisma <56932851+clarisma@users.noreply.github.com>
2025-03-17 18:04:48 +0100

    Update Math_test.cpp

--- test/common/math/Math_test.cpp
+++ test/common/math/Math_test.cpp
@@ -29,14 +29,23 @@ TEST_CASE("Math::parseDouble")
 	REQUIRE(success);
 	REQUIRE(d == 1977);
 
+	success = Math::parseDouble("", &d);
+	REQUIRE(!success);
+	REQUIRE(isnan(d));
+
+	success = Math::parseDouble("not_a_number", &d);
+	REQUIRE(!success);
+	REQUIRE(isnan(d));
+
 	success = Math::parseDouble("-monkey", &d);
 	REQUIRE(!success);
+	REQUIRE(isnan(d));
 
 	success = Math::parseDouble("..1", &d);
 	REQUIRE(!success);
+	REQUIRE(isnan(d));
 
 	success = Math::parseDouble("--20", &d);
 	REQUIRE(!success);
-
-
+	REQUIRE(isnan(d));
 }
